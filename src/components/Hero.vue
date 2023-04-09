919257183c0e4c946b3d0527324d2e04e2c5f330
Benno <bennokolar@gmail.com>
2023-04-09 21:00:52 +0200

    added image as url

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,7 +2,8 @@
 </script>
 
 <template>
-    <div class="hero min-h-screen" style="background-image: url('/src/assets/skyline.jpg');">
+    <div class="hero min-h-screen"
+        style="background-image: url(https://cdn.pixabay.com/photo/2016/11/19/15/03/buildings-1839726_960_720.jpg);">
         <div class="hero-overlay bg-opacity-30"></div>
         <div class="hero-content text-center text-neutral-content mt-96">
             <div class="max-w-md">
